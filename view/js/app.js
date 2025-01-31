c1c5b96eb99fc93e38f913fe7d35a240a01beb90
Keneil-Samms <keneil@innovate10x.com>
2025-01-31 15:21:09 -0500

    added Hero files

--- view/js/app.js
+++ view/js/app.js
@@ -1,12 +1,13 @@
 import Nav from "./components/nav.js";
-import getAttributes from "../../src/JSX PARSER/jsx-parser.js";
+// import getAttributes from "../../src/JSX PARSER/jsx-parser.js";
 import Render from "../../src/RenderComponent.js";
+import Hero from "./components/hero.js";
 const app = () => {
   Render([
-    Nav("props passed"),
-    
+    // Nav("props passed"),
+    Hero()
   ])
 };
 app();
 
-console.log(getAttributes());
+// console.log(getAttributes());
