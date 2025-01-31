716193a5a6c64a650917824dde9cc483490ad4e2
Keneil-Samms <keneil@innovate10x.com>
2025-01-31 15:19:36 -0500

    able to parse html into my custom js object

--- src/RenderComponent.js
+++ src/RenderComponent.js
@@ -1,11 +1,19 @@
+import CreateElement from "./CreateElement.js";
+import JSKLexer from "./JSX_PARSER/JSKLEXER.js";
+import JSKPARSER from "./JSX_PARSER/jsk-parser.js";
 export default function Render(components){
 const  Rootdiv = document.getElementById("root");
-const Handle={
-   apply:function(fn,Property,args){
-     return[Property,args];
-   }
-}
-components.forEach(element => {
-   Rootdiv.appendChild(element); 
-});
+
+ components.forEach(component=>{
+const tokenizer=new JSKLexer(component);
+  const tokens = tokenizer.tokenizer(component);
+  const jskParser=new JSKPARSER(tokens);
+   const domTree=jskParser.parser();
+
+   const element= new CreateElement(domTree,null);
+
+   console.log('domTree',domTree);
+   
+   Rootdiv.appendChild(element.create()); 
+ })
 }
